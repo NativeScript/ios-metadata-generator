5b4c74fad754489fb2b5f6d1477c499a630feabc
Иван Бухов <ivanbuhov@gmail.com>
2015-06-25 09:46:58 +0300

    Introduce tokens processors

--- src/RemoveUnsupportedSyntaxAction.cpp
+++ src/RemoveUnsupportedSyntaxAction.cpp
@@ -4,24 +4,234 @@
 
 #include "RemoveUnsupportedSyntaxAction.h"
 
-void RemoveUnsupportedSyntaxActionPPCallbacks::FileChanged(clang::SourceLocation Loc, FileChangeReason Reason, clang::SrcMgr::CharacteristicKind FileType, clang::FileID PrevFID) {
-        std::string newFile = action->preprocessor->getSourceManager().getFilename(Loc).str();
-        if(Reason == FileChangeReason::EnterFile && action->filesMap.find(newFile) == action->filesMap.end()) {
-                if(this->includeDirectives.size() > 0)
-                        action->filesMap[action->currentFileName] << std::string("\n") << this->includeDirectives[this->includeDirectives.size() - 1];
-                action->filesMap.insert(std::pair<std::string, std::stringstream>(newFile, std::stringstream()));
+std::string GenericDeclarationsProcessor::process(clang::Token& token) {
+    switch(stateInfo.state) {
+        case State::None : {
+            if(token.is(clang::tok::TokenKind::at))
+                stateInfo.state = State::RecognitionPhase1;
+            else
+                stateInfo.clear();
+            return next(token);
         }
-        action->currentFileName = newFile;
+        case State::RecognitionPhase1 : {
+            if(token.is(clang::tok::TokenKind::identifier) && preprocessor.getSpelling(token) == "interface")
+                stateInfo.state = State::RecognitionPhase2;
+            else
+                stateInfo.clear();
+            return next(token);
+        }
+        case State::RecognitionPhase2 : {
+            if(token.is(clang::tok::TokenKind::identifier)) {
+                stateInfo.genericInterfaceName = preprocessor.getSpelling(token);
+                stateInfo.state = State::RecognitionPhase3;
+            } else {
+                stateInfo.clear();
+            }
+            return next(token);
+        }
+        case State::RecognitionPhase3 : {
+            if(token.is(clang::tok::TokenKind::less)) {
+                std::vector<std::string>* genericDeclarations = getFromContext<std::vector<std::string>>(GenericsUsagesProcessor::GenericInterfacesKey);
+                if(genericDeclarations != nullptr)
+                    genericDeclarations->push_back(stateInfo.genericInterfaceName);
+                stateInfo.state = State::InAngleBrackets;
+                return " ";
+            } else {
+                stateInfo.clear();
+                return next(token);
+            }
+        }
+        case State::InAngleBrackets : {
+            if(token.is(clang::tok::TokenKind::less))
+                stateInfo.angleBracketsLevel++;
+            else if(token.is(clang::tok::TokenKind::greater))
+                stateInfo.angleBracketsLevel--;
+            else if(token.is(clang::tok::TokenKind::lessless))
+                stateInfo.angleBracketsLevel += 2;
+            else if(token.is(clang::tok::TokenKind::greatergreater))
+                stateInfo.angleBracketsLevel -= 2;
+            else if(token.is(clang::tok::TokenKind::identifier))
+                stateInfo.genericParameterName = preprocessor.getSpelling(token);
+            if(stateInfo.angleBracketsLevel == -1) {
+                stateInfo.angleBracketsLevel = 0;
+                stateInfo.state = State::InDeclaration;
+            }
+//            static int i(0);
+//            i++;
+//            printf("%s [%s]\n", preprocessor.getSpelling(token).c_str(), token.getLocation().printToString(preprocessor.getSourceManager()).c_str());
+//            if(i % 10 == 0) {
+//                printf("breakpoint\n");
+//            }
+            return "";
+        }
+        case State::InDeclaration : {
+            if(token.is(clang::tok::TokenKind::at))
+                stateInfo.state = State::EndRecognitionPhase1;
+            else if(token.is(clang::tok::TokenKind::identifier) && preprocessor.getSpelling(token) == stateInfo.genericParameterName) {
+                std::string result = next(token);
+                return (result.empty()) ? result : "id";
+            }
+            return next(token);
+        }
+        case State::EndRecognitionPhase1 : {
+            if(token.is(clang::tok::TokenKind::identifier) && preprocessor.getSpelling(token) == "end")
+                stateInfo.clear();
+            else
+                stateInfo.state = State::InDeclaration;
+            return next(token);
+        }
+    }
+}
+
+const std::string GenericsUsagesProcessor::GenericInterfacesKey("GenericsUsagesProcessor.GenericInterfacesKey");
+
+std::string GenericsUsagesProcessor::process(clang::Token& token) {
+    switch(stateInfo.state) {
+        case State::None : {
+            if(token.is(clang::tok::TokenKind::identifier)) {
+                std::string tokenSpelling = preprocessor.getSpelling(token);
+                std::vector<std::string>* genericInterfaces = getFromContext<std::vector<std::string>>(GenericsUsagesProcessor::GenericInterfacesKey);
+                if(genericInterfaces != nullptr && std::find(genericInterfaces->begin(), genericInterfaces->end(), tokenSpelling) != genericInterfaces->end())
+                    stateInfo.state = State::RecognitionPhase1;
+            }
+            else if(token.is(clang::tok::TokenKind::colon)) {
+                stateInfo.state = State::AfterColon;
+            }
+            return next(token);
+        }
+        case State::AfterColon : {
+            if(!token.is(clang::tok::TokenKind::colon))
+                stateInfo.state = State::None;
+            return next(token);
+        }
+        case State::RecognitionPhase1 : {
+            if(token.is(clang::tok::TokenKind::less)) {
+                stateInfo.state = State::InAngleBrackets;
+                return " ";
+            } else {
+                stateInfo.clear();
+                return next(token);
+            }
+        }
+        case State::InAngleBrackets : {
+            if(token.is(clang::tok::TokenKind::less))
+                stateInfo.angleBracketsLevel++;
+            else if(token.is(clang::tok::TokenKind::greater))
+                stateInfo.angleBracketsLevel--;
+            else if(token.is(clang::tok::TokenKind::lessless))
+                stateInfo.angleBracketsLevel += 2;
+            else if(token.is(clang::tok::TokenKind::greatergreater))
+                stateInfo.angleBracketsLevel -= 2;
+            if(stateInfo.angleBracketsLevel == -1) {
+                stateInfo.clear();
+            }
+            return "";
+        }
+    }
+}
+
+std::string GenericsForwardDeclarationsProcessor::process(clang::Token& token) {
+    switch(stateInfo.state) {
+        case State::None : {
+            if(token.is(clang::tok::TokenKind::at))
+                stateInfo.state = State::RecognitionPhase1;
+            else
+                stateInfo.clear();
+            return next(token);
+        }
+        case State::RecognitionPhase1 : {
+            if(token.is(clang::tok::TokenKind::identifier) && preprocessor.getSpelling(token) == "class")
+                stateInfo.state = State::RecognitionPhase2;
+            else
+                stateInfo.clear();
+            return next(token);
+        }
+        case State::RecognitionPhase2 : {
+            if(token.is(clang::tok::TokenKind::identifier)) {
+                stateInfo.state = State::RecognitionPhase3;
+                stateInfo.genericInterfaceName = preprocessor.getSpelling(token);
+            } else {
+                stateInfo.clear();
+            }
+            return next(token);
+        }
+        case State::RecognitionPhase3 : {
+            if(token.is(clang::tok::TokenKind::less)) {
+                stateInfo.state = State::InAngleBrackets;
+                std::vector<std::string> *genericsNames = getFromContext<std::vector<std::string>>(GenericsUsagesProcessor::GenericInterfacesKey);
+                if(genericsNames != nullptr && std::find(genericsNames->begin(), genericsNames->end(), stateInfo.genericInterfaceName) == genericsNames->end()) {
+                    genericsNames->push_back(stateInfo.genericInterfaceName);
+                }
+                return "";
+            } else {
+                stateInfo.clear();
+                return next(token);
+            }
+        }
+        case State::InAngleBrackets : {
+            if(token.is(clang::tok::TokenKind::less))
+                stateInfo.angleBracketsLevel++;
+            else if(token.is(clang::tok::TokenKind::greater))
+                stateInfo.angleBracketsLevel--;
+            else if(token.is(clang::tok::TokenKind::lessless))
+                stateInfo.angleBracketsLevel += 2;
+            else if(token.is(clang::tok::TokenKind::greatergreater))
+                stateInfo.angleBracketsLevel -= 2;
+            if(stateInfo.angleBracketsLevel == -1) {
+                stateInfo.clear();
+            }
+            return "";
+        }
+    }
+}
+
+std::string NullabilityModifiersProcessor::process(clang::Token& token) {
+    switch(stateInfo.state) {
+        case State::None : {
+            if(token.is(clang::tok::TokenKind::identifier)) {
+                std::string spelling = preprocessor.getSpelling(token);
+                if(spelling == "nonnull" || spelling == "nullable" || spelling == "__null_unspecified") {
+                    stateInfo.state = State::AfterNullabilityModifier;
+                    return "";
+                }
+            }
+            return next(token);
+        }
+        case State::AfterNullabilityModifier : {
+            stateInfo.state = State::None;
+            return token.is(clang::tok::TokenKind::comma) ? "" : next(token);
+        }
+    }
+
+
+
+    return next(token);
+}
+
+std::string DefaultProcessor::process(clang::Token& token) {
+    bool isTokenInvalid = false;
+    std::string spelling = preprocessor.getSpelling(token, &isTokenInvalid);
+    return isTokenInvalid ? "" : (std::string(token.isAtStartOfLine() ? "\n" : "") + (token.hasLeadingSpace() ? " " : "") + spelling);
+}
+
+void RemoveUnsupportedSyntaxActionPPCallbacks::FileChanged(clang::SourceLocation Loc, FileChangeReason Reason, clang::SrcMgr::CharacteristicKind FileType, clang::FileID PrevFID) {
+    std::string newFile = action->preprocessor->getSourceManager().getFilename(Loc).str();
+    if(Reason == FileChangeReason::EnterFile && action->filesMap.find(newFile) == action->filesMap.end()) {
+        if(this->includeDirectives.size() > 0)
+            action->filesMap[action->currentFileName] << std::string("\n") << this->includeDirectives[this->includeDirectives.size() - 1];
+        action->filesMap.insert(std::pair<std::string, std::stringstream>(newFile, std::stringstream()));
+    }
+    action->currentFileName = newFile;
 }
 
 void RemoveUnsupportedSyntaxActionPPCallbacks::InclusionDirective(clang::SourceLocation HashLoc, const clang::Token &IncludeTok, clang::StringRef FileName,
                                                                   bool IsAngled, clang::CharSourceRange FilenameRange, const clang::FileEntry *File,
                                                                   clang::StringRef SearchPath, clang::StringRef RelativePath, const clang::Module *Imported) {
-        std::string openingSymbol = IsAngled ? "<" : "\"";
-        std::string closingSymbol = IsAngled ? ">" : "\"";
-        std::stringstream includeDirective;
-        includeDirective << "#" << action->preprocessor->getSpelling(IncludeTok) << " " << openingSymbol << FileName.str() << closingSymbol;
-        this->includeDirectives.push_back(includeDirective.str());
+    std::string openingSymbol = IsAngled ? "<" : "\"";
+    std::string closingSymbol = IsAngled ? ">" : "\"";
+    std::stringstream includeDirective;
+    includeDirective << "#" << action->preprocessor->getSpelling(IncludeTok) << " " << openingSymbol << FileName.str() << closingSymbol;
+    this->includeDirectives.push_back(includeDirective.str());
 }
 
 void RemoveUnsupportedSyntaxActionPPCallbacks::EndOfMainFile() {
@@ -29,29 +239,30 @@ void RemoveUnsupportedSyntaxActionPPCallbacks::EndOfMainFile() {
 }
 
 bool RemoveUnsupportedSyntaxAction::BeginSourceFileAction(clang::CompilerInstance &CI, clang::StringRef Filename) {
-        // setup preprocessor
-        CI.getPreprocessor().addPPCallbacks(std::unique_ptr<clang::PPCallbacks>(new RemoveUnsupportedSyntaxActionPPCallbacks(this)));
-        this->preprocessor = &CI.getPreprocessor();
-        const clang::FileEntry *mainFileEntry = preprocessor->getFileManager().getFile(Filename);
-        clang::FileID fileId = preprocessor->getSourceManager().createFileID(mainFileEntry, clang::SourceLocation(), clang::SrcMgr::CharacteristicKind::C_User);
-        preprocessor->getSourceManager().setMainFileID(fileId);
-        preprocessor->EnterMainSourceFile();
+    // setup preprocessor
+    CI.getPreprocessor().addPPCallbacks(std::unique_ptr<clang::PPCallbacks>(new RemoveUnsupportedSyntaxActionPPCallbacks(this)));
+    this->preprocessor = &CI.getPreprocessor();
+    const clang::FileEntry *mainFileEntry = preprocessor->getFileManager().getFile(Filename);
+    clang::FileID fileId = preprocessor->getSourceManager().createFileID(mainFileEntry, clang::SourceLocation(), clang::SrcMgr::CharacteristicKind::C_User);
+    preprocessor->getSourceManager().setMainFileID(fileId);
+    preprocessor->EnterMainSourceFile();
 
-        // parse tokens
-        clang::Token token;
-        bool isTokenInvalid = false;
-        do {
-                preprocessor->Lex(token);
-                if(preprocessor->getDiagnostics().hasErrorOccurred()) {
-                        break;
-                }
+    std::map<std::string, void*> context;
+    GenericDeclarationsProcessor tokensProcessor(*preprocessor, context,
+    std::make_shared<NullabilityModifiersProcessor>(*preprocessor, context,
+    std::make_shared<GenericsUsagesProcessor>(*preprocessor, context,
+    std::make_shared<GenericsForwardDeclarationsProcessor>(*preprocessor, context,
+    std::make_shared<DefaultProcessor>(*preprocessor, context, nullptr)))));
+    //GenericDeclarationsProcessor tokensProcessor(*preprocessor, context, std::make_shared<DefaultProcessor>(*preprocessor, context, nullptr));
 
-                std::string spelling = preprocessor->getSpelling(token, &isTokenInvalid);
-                if(isTokenInvalid) {
-                        preprocessor->DumpToken(token);
-                }
-                this->filesMap[currentFileName] << (token.isAtStartOfLine() ? "\n" : "") << (token.hasLeadingSpace() ? " " : "") << spelling;
-        } while(token.isNot(clang::tok::eof));
+    // parse tokens
+    clang::Token token;
+    do {
+        preprocessor->Lex(token);
+        if(preprocessor->getDiagnostics().hasErrorOccurred())
+            break;
+        this->filesMap[currentFileName] << tokensProcessor.process(token);
+    } while(token.isNot(clang::tok::eof));
 
-        return true;
+    return true;
 }
--- src/RemoveUnsupportedSyntaxAction.h
+++ src/RemoveUnsupportedSyntaxAction.h
@@ -5,11 +5,168 @@
 #pragma once
 
 #include <sstream>
+#include <deque>
 #include <clang/Lex/Preprocessor.h>
 #include <clang/Frontend/FrontendActions.h>
 #include <clang/Frontend/CompilerInstance.h>
 
 class RemoveUnsupportedSyntaxAction;
+
+class TokensProcessor {
+protected:
+    clang::Preprocessor& preprocessor;
+    std::shared_ptr<TokensProcessor> nextProcessor;
+    std::map<std::string, void*>& context;
+
+    std::string next(clang::Token& token) {
+        return nextProcessor ? nextProcessor->process(token) : "";
+    }
+
+public:
+    TokensProcessor(clang::Preprocessor& preprocessor, std::map<std::string, void*>& context, std::shared_ptr<TokensProcessor> next = nullptr)
+            : preprocessor(preprocessor),
+              context(context),
+              nextProcessor(next) {}
+
+    virtual std::string process(clang::Token& token) = 0;
+
+    template<class T>
+    T* getFromContext(std::string key) {
+        auto it = context.find(key);
+        return (it == context.end()) ? nullptr : static_cast<T*>(it->second);
+    }
+
+    void addToContext(std::string key, void* value) {
+        context[key] = value;
+    }
+};
+
+class GenericDeclarationsProcessor : public TokensProcessor {
+    enum State {
+        None = 0,
+        RecognitionPhase1, // @ (searching for "@interface identifier<")
+        RecognitionPhase2, // @interface
+        RecognitionPhase3, // @interface identifier
+        InAngleBrackets, // @interface identifier<
+        InDeclaration,
+        EndRecognitionPhase1 // @ (searching for "@end")
+    };
+
+    struct StateInfo {
+        State state = State::None;
+        int angleBracketsLevel = 0;
+        std::string genericInterfaceName;
+        std::string genericParameterName;
+
+        void clear() {
+            state = State::None;
+            angleBracketsLevel = 0;
+            genericInterfaceName = "";
+            genericParameterName = "";
+        }
+    };
+
+private:
+    StateInfo stateInfo;
+public:
+
+    GenericDeclarationsProcessor(clang::Preprocessor& preprocessor, std::map<std::string, void*>& context, std::shared_ptr<TokensProcessor> next = nullptr)
+            : TokensProcessor(preprocessor, context, next) {}
+
+    virtual std::string process(clang::Token& token) override;
+};
+
+class GenericsUsagesProcessor : public TokensProcessor {
+    enum State {
+        None = 0,
+        AfterColon,
+        RecognitionPhase1, // @ (searching for "identifier<")
+        InAngleBrackets // identifier<
+    };
+
+    struct StateInfo {
+        State state = State::None;
+        int angleBracketsLevel = 0;
+
+        void clear() {
+            state = State::None;
+            angleBracketsLevel = 0;
+        }
+    };
+
+private:
+    StateInfo stateInfo;
+public:
+    GenericsUsagesProcessor(clang::Preprocessor& preprocessor, std::map<std::string, void*>& context, std::shared_ptr<TokensProcessor> next = nullptr)
+            : TokensProcessor(preprocessor, context, next) {
+        addToContext(GenericsUsagesProcessor::GenericInterfacesKey, new std::vector<std::string>);
+    }
+
+    static const std::string GenericInterfacesKey;
+
+    virtual std::string process(clang::Token& token) override;
+};
+
+class GenericsForwardDeclarationsProcessor : public TokensProcessor {
+    enum State {
+        None = 0,
+        RecognitionPhase1, // @ (searching for "@class identifier<..>")
+        RecognitionPhase2, // @class (searching for "@class identifier<..>")
+        RecognitionPhase3, // @class identifier (searching for "@class identifier<..>")
+        InAngleBrackets, // @class identifier<...> (searching for "@class identifier<..>")
+    };
+
+    struct StateInfo {
+        State state = State::None;
+        std::string genericInterfaceName;
+        int angleBracketsLevel = 0;
+
+        void clear() {
+            state = State::None;
+            genericInterfaceName = "";
+            angleBracketsLevel = 0;
+        }
+    };
+
+private:
+    StateInfo stateInfo;
+public:
+    GenericsForwardDeclarationsProcessor(clang::Preprocessor& preprocessor, std::map<std::string, void*>& context, std::shared_ptr<TokensProcessor> next = nullptr)
+            : TokensProcessor(preprocessor, context, next) { }
+
+    virtual std::string process(clang::Token& token) override;
+};
+
+class NullabilityModifiersProcessor : public TokensProcessor {
+    enum State {
+        None = 0,
+        AfterNullabilityModifier
+    };
+
+    struct StateInfo {
+        State state = State::None;
+
+        void clear() {
+            state = State::None;
+        }
+    };
+
+private:
+    StateInfo stateInfo;
+public:
+    NullabilityModifiersProcessor(clang::Preprocessor& preprocessor, std::map<std::string, void*>& context, std::shared_ptr<TokensProcessor> next = nullptr)
+            : TokensProcessor(preprocessor, context, next) { }
+
+    virtual std::string process(clang::Token& token) override;
+};
+
+class DefaultProcessor : public TokensProcessor {
+public:
+    DefaultProcessor(clang::Preprocessor& preprocessor, std::map<std::string, void*>& context, std::shared_ptr<TokensProcessor> next = nullptr)
+            : TokensProcessor(preprocessor, context, next) {}
+
+    virtual std::string process(clang::Token& token) override;
+};
 
 class RemoveUnsupportedSyntaxActionPPCallbacks : public clang::PPCallbacks {
 private:
