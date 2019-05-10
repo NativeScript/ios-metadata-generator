a1cf494bed7d1c9abb9b8e6ffb99f3c337d818de
Martin Bektchiev <martin.bektchiev@progress.com>
2019-05-10 11:35:26 +0300

    fix: Stop randomizing modules' order of serialization

--- src/Meta/Filters/ResolveGlobalNamesCollisionsFilter.h
+++ src/Meta/Filters/ResolveGlobalNamesCollisionsFilter.h
@@ -23,7 +23,19 @@ public:
         std::unique_ptr<std::pair<MetasByModules, InterfacesByName> > result = llvm::make_unique<std::pair<MetasByModules, InterfacesByName> >(MetasByModules(), InterfacesByName());
         MetasByModules& metasByModules = result->first;
         InterfacesByName& interfacesByName = result->second;
-        for (auto& module : _modules) {
+        
+        // Sort modules by name to avoid randomizing metadata
+        typedef std::vector<ModulesStructure::value_type*> ModulesVector;
+        ModulesVector v;
+        std::transform(_modules.begin(), _modules.end(), std::back_inserter(v), [](ModulesStructure::value_type& it) {
+            return &it;
+        });
+        std::sort(v.begin(), v.end(), [](const ModulesVector::value_type& a, const ModulesVector::value_type&  b) {
+            return a->first->Name.compare(b->first->Name) < 0;
+        });
+
+        for (auto& mptr : v) {
+            auto& module = *mptr;
             std::pair<clang::Module*, std::vector<Meta*> > modulePair(module.first, std::vector<Meta*>());
             for (const std::pair<std::string, std::vector<Meta*> >& metas : module.second) {
                 assert(metas.second.size() == 1);
